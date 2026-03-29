2de9a88ac94874684b73253bba35b7fd10f84ca3
MBurleson33 <114445009+MBurleson33@users.noreply.github.com>
2026-03-29 08:17:42 -0500

    Update robots.js

--- api/robots.js
+++ api/robots.js
@@ -1,8 +1,8 @@
-export default function handler(req, res) {
+module.exports = function handler(req, res) {
   res.setHeader('Content-Type', 'text/plain');
   res.setHeader('Cache-Control', 's-maxage=86400');
   res.status(200).send(`User-agent: *
 Allow: /
 
 Sitemap: https://brokenandburied.com/sitemap.xml`);
-}
+};
